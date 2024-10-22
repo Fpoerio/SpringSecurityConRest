75fe390b2c6e72a35234feece92eeeb67a9f58c4
Fpoerio <f.poerio@dotitsrl.com>
2024-10-22 14:37:28 +0200

    ultimo salvataggio

--- demo/src/main/java/it/dotit/demo/service/Scheduler.java
+++ demo/src/main/java/it/dotit/demo/service/Scheduler.java
@@ -1,23 +1,15 @@
 package it.dotit.demo.service;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-
-
 import io.jsonwebtoken.io.IOException;
-
-
 
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -25,25 +17,20 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-
-
 import java.util.List;
 import java.util.ArrayList;
 
 @Component
 public class Scheduler {
-	
 
 	private static final Logger logger = LoggerFactory.getLogger(CsvService.class);
-	
-	//(fixedRate = 5000)(cron = "0 0 12 * * ?")
-	//@Scheduled(cron = "0 0 12 * * ?")
-	//@Async
-	//(cron = "0 0 1 * * ?")//una volta al mese
 
+	// (fixedRate = 5000)(cron = "0 0 12 * * ?")
+	// @Scheduled(cron = "0 0 12 * * ?")
+	// @Async
+	// (cron = "0 0 1 * * ?")//una volta al mese
 
-
-    private final String pathTxt = "C:\\Users\\Fabio\\git\\springSecurityRest\\demo\\src\\main\\resources\\txtCsvEliminati.txt";
+	private final String pathTxt = "C:\\Users\\Fabio\\git\\springSecurityRest\\demo\\src\\main\\resources\\txtCsvEliminati.txt";
 
 	@Scheduled(cron = "0 0 12 * * ?") // Esegue il controllo ogni giorno a mezzogiorno
 	private void removeOldRecords() throws FileNotFoundException, java.io.IOException {
@@ -66,17 +53,20 @@ public class Scheduler {
 
 				if (removalDateString != null) {
 					try {
-						LocalDate removalDate = LocalDate.parse(removalDateString, formatter); // Converte la stringa in LocalDate
+						LocalDate removalDate = LocalDate.parse(removalDateString, formatter); // Converte la stringa in
+																								// LocalDate
 						if (removalDate.isAfter(thresholdDate)) {
 							linesToKeep.add(line); // Aggiunge la riga se la data è recente
 						}
 					} catch (DateTimeParseException e) {
-						logger.error("Formato data non valido: {}", removalDateString); // Log di errore per formato non valido
+						logger.error("Formato data non valido: {}", removalDateString); // Log di errore per formato non
+																						// valido
 					}
 				}
 			}
 		} catch (IOException e) {
-			logger.error("Errore durante la lettura del file: {}", e.getMessage()); // Log di errore per problemi di lettura
+			logger.error("Errore durante la lettura del file: {}", e.getMessage()); // Log di errore per problemi di
+																					// lettura
 		}
 
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(pathTxt))) {
@@ -85,17 +75,11 @@ public class Scheduler {
 				writer.newLine();
 			}
 		} catch (IOException e) {
-			logger.error("Errore durante la scrittura nel file: {}", e.getMessage()); // Log di errore per problemi di scrittura
+			logger.error("Errore durante la scrittura nel file: {}", e.getMessage()); // Log di errore per problemi di
+																						// scrittura
 		}
 
 		logger.info("Operazione completata. Righe vecchie rimosse."); // Log di completamento
 	}
 
-
-
-	
 }
-
-	
-	
-
