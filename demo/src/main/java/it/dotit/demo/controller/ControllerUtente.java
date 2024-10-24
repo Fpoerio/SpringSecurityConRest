68575bd6cf7fa7af280bb8a7ea76bbfa061bb3df
Fpoerio <f.poerio@dotitsrl.com>
2024-10-24 12:20:45 +0200

    24/10/2024 correzioni e swagger
    Signed-off-by: Fpoerio <f.poerio@dotitsrl.com>

--- demo/src/main/java/it/dotit/demo/controller/ControllerUtente.java
+++ demo/src/main/java/it/dotit/demo/controller/ControllerUtente.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import it.dotit.demo.auth.AuthenticationResponse;
 import it.dotit.demo.auth.RegisterRequest;
 import it.dotit.demo.auth.UpdateUserRequest;
+import it.dotit.demo.exceptions.myExceptions.UserNotFoundException;
 import it.dotit.demo.model.Role;
 import it.dotit.demo.service.AuthenticationService;
 import it.dotit.demo.service.CrudUserService;
